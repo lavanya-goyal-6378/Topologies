ef070937186d8bb899bb64c8ef24cbc8a7c66a59
lavanya-goyal-6378 <lavanyagoel2004@gmail.com>
2026-04-16 03:30:53 +0000

    Initial commit of Fat Tree topology

---
+++ fat_tree.py
@@ -0,0 +1,44 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+
+class FatTree4(Topo):
+	def build(self):
+		k=4
+		num_pods=k
+		num_core_switches = (k//2) ** 2
+		cores=[]
+		for i in range(num_core_switches):
+            		cores.append(self.addSwitch(f'c{i+1}'))
+		for p in range(num_pods):
+			aggs=[]
+			edges=[]
+			for i in range (k//2):
+				aggs.append(self.addSwitch(f'p{p}_a{i+1}'))
+				edges.append(self.addSwitch(f'p{p}_e{i+1}'))
+			for a in aggs:
+                		for c in cores:
+                    			self.addLink(a, c)
+			for e in edges:
+				for a in aggs:
+					self.addLink(e,a)
+			for i,e in enumerate(edges):
+				for h_idx in range(k//2):
+					h= self.addHost(f'p{p}_e{i+1}_h{h_idx+1}')
+					self.addLink(h,e)
+
+def run ():
+	topo = FatTree4()
+	net= Mininet(topo = topo, autoStaticArp=True)
+	net.start()
+	print("Enabling STP to prevent loops...")
+	for sw in net.switches:
+		sw.cmd('ovs-vsctl set bridge', sw.name, 'stp_enable=true')
+	import time
+	time.sleep(2)
+	CLI(net)
+	net.stop()
+if __name__ == '__main__':
+	setLogLevel('info')
+	run()
